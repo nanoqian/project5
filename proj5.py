e3de8612e345a3dd8fce2168fd833a6c214adabf
nanoqian <maike@nanoqian.com>
2025-01-26 14:50:28 -0500

    Added basic graphing

--- proj5.py
+++ proj5.py
@@ -1,1 +1,19 @@
 # Project 05
+
+# Electric field visualization
+
+import sympy as sp
+import math
+
+# Symbols
+
+t = sp.symbols('t')
+
+# Operation
+
+f = sp.sin(t)
+g = sp.tan(t)
+
+
+# Plot this
+p = sp.plot_parametric(f,g, (t,0, 2*sp.pi))
