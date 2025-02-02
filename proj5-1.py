e70cbba8cb0b1056ada64ffe30d0eff6c7fd8dd5
nanoqian <maike@nanoqian.com>
2025-02-02 17:30:30 -0500

    Proximity limit and higher res graph

--- proj5-1.py
+++ proj5-1.py
@@ -6,21 +6,18 @@ class Charge:
     def __init__(self, x: float, y: float, charge: float) -> None:
         self.x = x
         self.y = y
-        self.energy = charge
+        self.charge = charge
 
-def field_from_charge(X, Y, charge: Charge):
-    # Make each point a unit vector pointing away from the charge
+def field_from_charge(X, Y, charge: Charge, epsilon=1e-12, min_distance=0.5):
     U = X - charge.x
     V = Y - charge.y
     magnitudes = np.sqrt(U**2 + V**2)
+    magnitudes = np.where(magnitudes < min_distance, min_distance, magnitudes)
     U /= magnitudes
     V /= magnitudes
-    
-    # Scale the vectors by the energy from the charge
-    energy = charge.energy / ((X - charge.x)**2 + (Y - charge.y)**2)
+    energy = charge.charge / (magnitudes**2)
     U *= energy
     V *= energy
-    
     return U, V
 
 def add_tuple(A, B):
@@ -29,11 +26,12 @@ def add_tuple(A, B):
     return Ax + Bx, Ay + By
 
 def field_from_charges(X, Y, charges: list[Charge]):
-    # The field from multiple charges is the sum of the field from each charge
     return reduce(add_tuple, map(partial(field_from_charge, X, Y), charges))
 
-X, Y = np.meshgrid(np.linspace(-10, 10, 20), np.linspace(-10, 10, 20))
-U, V = field_from_charges(X, Y, [Charge(-5, 0, 1), Charge(5, 0, -1)])
+X, Y = np.meshgrid(np.linspace(-10, 10, 200), np.linspace(-10, 10, 200))
 
-plt.quiver(X, Y, U, V)
+charges = [Charge(-5, 0, 1), Charge(5, 0, -1)]
+U, V = field_from_charges(X, Y, charges)
+
+plt.quiver(X, Y, U, V, scale=30)
 plt.show()
